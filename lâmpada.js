e20fc631427bd0a9417a7e95a71d838130f4110f
resutmarcos <110950921+resutmarcos@users.noreply.github.com>
2022-08-11 11:22:30 -0300

    condiçoes novas

--- lâmpada.js
+++ lâmpada.js
@@ -2,12 +2,24 @@ const turnOn = document.getElementById ( 'turnOn' );
 const turnOff = document.getElementById ( 'turnOff' );
 const lamp = document.getElementById ( 'lamp' );
 
+function islampBroken (){
+    return lamp.src.indexOf ('quebrada') > -1;
+}
+
 function lampOn () {
-    lamp.src = './ligada.jpg';
+    if ( !islampBroken () ){
+      lamp.src = './ligada.jpg';
+    }
 }
 function lampOff () {
-    lamp.src = './desligada.jpg';
+    if ( !islampBroken () ){
+       lamp.src = './desligada.jpg';
+    }
+}
+function lampBroken(){
+    lamp.src = './quebrada.jpg'
 }
 
 turnOn.addEventListener ( "click", lampOn);
 turnOff.addEventListener ( "click", lampOff);
+lamp.addEventListener ('click', lampBroken);
